53e988a83e83ea7bfcad79b392eefb425b9d7e34
bhinmantx <brendan.hinman@gmail.com>
2013-12-03 11:49:35 -0600

    Added a bunch of includes, moved some variables around. This gets a preview working

--- IsItGreen/IsItGreenViewController.h
+++ IsItGreen/IsItGreenViewController.h
@@ -7,7 +7,27 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVCaptureSession.h>
+#import <AVFoundation/AVCaptureDevice.h>
+#import <AVFoundation/AVCaptureInput.h>
+#import <AVFoundation/AVCaptureOutput.h>
+#import <AVFoundation/AVCaptureVideoPreviewLayer.h>
+#import <AVFoundation/AVFoundation.h>
+
 
 @interface IsItGreenViewController : UIViewController
+{
+    IBOutlet UIView *cameraFeed;
+    
+    
+  
+}
+
+
+@property(nonatomic, retain) IBOutlet UIView *cameraFeed;
+
+/////below is for still image capture
+@property(nonatomic, retain) AVCaptureStillImageOutput *stillImageOutput;
+@property (weak, nonatomic) IBOutlet UIImageView *captureImage;
 
 @end
--- IsItGreen/IsItGreenViewController.mm
+++ IsItGreen/IsItGreenViewController.mm
@@ -8,16 +8,144 @@
 
 #import "IsItGreenViewController.h"
 
+
 @interface IsItGreenViewController ()
 
 @end
 
 @implementation IsItGreenViewController
 
+@synthesize cameraFeed;
+
+///For still image capture
+@synthesize stillImageOutput, captureImage;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    AVCaptureSession *session = [[AVCaptureSession alloc] init];
+    
+  CALayer *viewLayer = self.cameraFeed.layer;
+    AVCaptureVideoPreviewLayer *captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
+    
+    CGRect bounds=self.cameraFeed.layer.bounds;
+    captureVideoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    captureVideoPreviewLayer.bounds=bounds;
+    captureVideoPreviewLayer.position=CGPointMake(CGRectGetMidX(bounds), CGRectGetMidY(bounds));
+ 
+
+  captureVideoPreviewLayer.frame = viewLayer.bounds;
+    
+    
+    
+    
+    
+    
+    
+	[self.cameraFeed.layer addSublayer:captureVideoPreviewLayer];
+    
+    
+
+   // AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:@"AVMediaTypeVideo"];
+
+    
+    
+    
+    
+    NSArray *devices = [AVCaptureDevice devices];
+    AVCaptureDevice *frontCamera;
+    AVCaptureDevice *backCamera;
+    
+    for (AVCaptureDevice *device in devices) {
+        
+        NSLog(@"Device name: %@", [device localizedName]);
+        
+        if ([device hasMediaType:AVMediaTypeVideo]) {
+            
+            if ([device position] == AVCaptureDevicePositionBack) {
+                NSLog(@"Device position : back");
+                backCamera = device;
+            }
+            else {
+                NSLog(@"Device position : front");
+                frontCamera = device;
+            }
+        }
+    }
+    /*
+    if (!frontCamera) {
+        NSError *error = nil;
+        AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:backCamera error:&error];
+        if (!input) {
+            NSLog(@"ERROR: trying to open front camera: %@", error);
+        }
+        [session addInput:input];
+    }
+    
+    if (frontCamera) {
+        NSError *error = nil;
+        AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:frontCamera error:&error];
+        if (!input) {
+            NSLog(@"ERROR: trying to open rear camera: %@", error);
+        }
+        [session addInput:input];
+    }
+    */
+    
+    NSError *error = nil;
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:backCamera error:&error];
+    [session addInput:input];
+    
+   // NSDictionary *outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys: AVVideoCodecJPEG, AVVideoCodecKey, nil];
+
+    
+    //[session addOutput:stillImageOutput];
+    
+    
+    
+    stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+    NSDictionary *outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys: AVVideoCodecJPEG, AVVideoCodecKey, nil];
+    [stillImageOutput setOutputSettings:outputSettings];
+    
+    [session addOutput:stillImageOutput];
+    
+    
+    
+	[session startRunning];
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    NSError *error = nil;
+    
+	AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+	if (!input) {
+		// Handle the error appropriately.
+		NSLog(@"ERROR: trying to open camera: %@", error);
+	}
+    
+    [session startRunning];
+	
+    [session addInput:input];
+*/
+
+    
 }
 
 - (void)didReceiveMemoryWarning
