9eb1b13928951190634d20050b8b529653711748
bhinmantx <brendan.hinman@gmail.com>
2013-12-05 01:23:35 -0600

    Stuff is broken but I’m removing commented code to avoid confusion

--- IsItGreen/IsItGreenViewController.h
+++ IsItGreen/IsItGreenViewController.h
@@ -13,6 +13,7 @@
 #import <AVFoundation/AVCaptureOutput.h>
 #import <AVFoundation/AVCaptureVideoPreviewLayer.h>
 #import <AVFoundation/AVFoundation.h>
+#import "ImageUtils.h"
 
 
 @interface IsItGreenViewController : UIViewController <AVCaptureVideoDataOutputSampleBufferDelegate>
@@ -20,9 +21,12 @@
     IBOutlet UIView *cameraFeed;
     
     ////Image utility. Very expensive!
-
+    AVCaptureSession *session;
+    
     BOOL processVideoFrame;
    // dispatch_queue_t captureQueue;
+    //UIImage *thumbNail;
+    Image *thumbNail;
   
 }
 
@@ -30,13 +34,17 @@
 @property(nonatomic, retain) IBOutlet UIView *cameraFeed;
 
 /////below is for still image capture
-@property(nonatomic, retain) AVCaptureStillImageOutput *stillImageOutput;
+//@property(nonatomic, retain) AVCaptureStillImageOutput *stillImageOutput;
 @property (weak, nonatomic) IBOutlet UIImageView *captureImage;
 @property (strong, nonatomic) IBOutlet UIImageView *subImage;
+//@property (strong, nonatomic) UIImage *thumbNail;
+
 //@property (strong,nonatomic)dispatch_queue_t captureQueue;
 
 -(UIImage *)imageFromSampleBuffer:(CMSampleBufferRef)sampleBuffer;
 -(void)prepVidCapture;
+-(void)updateThumbnail;
+
 
 @property (strong, nonatomic) IBOutlet UIButton *TriggerButton;
 
--- IsItGreen/IsItGreenViewController.mm
+++ IsItGreen/IsItGreenViewController.mm
@@ -8,7 +8,7 @@
 
 #import "IsItGreenViewController.h"
 #import "AVFoundation/AVCaptureOutput.h"
-
+#import "ImageUtils.h"
 
 @interface IsItGreenViewController ()
 
@@ -18,21 +18,90 @@
 
 @synthesize cameraFeed, subImage;
 ///For still image capture
-@synthesize stillImageOutput, captureImage;
+@synthesize captureImage;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+      processVideoFrame = false;
     [self prepVidCapture];
 	// Do any additional setup after loading the view, typically from a nib.
 
-    processVideoFrame = false;
+  
 }
 
 
 ///Creates all the preview layers, adds inputs/outputs, registers the queue
 -(void)prepVidCapture{
-    AVCaptureSession *session = [[AVCaptureSession alloc] init];
+
+
+
+///Below is all of my broken code.  Example code being pasted in until I can figure out what's going on.
+
+
+
+    session = [[AVCaptureSession alloc] init];
+	
+    
+    CALayer *viewLayer = self.cameraFeed.layer;
+    AVCaptureVideoPreviewLayer *captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
+    
+    ///This should properly size and fill the preview layer
+    CGRect bounds=self.cameraFeed.layer.bounds;
+    captureVideoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    captureVideoPreviewLayer.bounds=bounds;
+    captureVideoPreviewLayer.position=CGPointMake(CGRectGetMidX(bounds), CGRectGetMidY(bounds));
+    
+    
+    captureVideoPreviewLayer.frame = viewLayer.bounds;
+    
+    [self.cameraFeed.layer addSublayer:captureVideoPreviewLayer];
+    
+    
+	// create a preview layer to show the output from the camera
+	//AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:session];
+	//previewLayer.frame = cameraFeed.frame;
+	//[cameraFeed.layer addSublayer:previewLayer];
+	
+	// Get the default camera device
+	AVCaptureDevice* camera = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+	
+	// Create a AVCaptureInput with the camera device
+	NSError *error=nil;
+	AVCaptureInput* cameraInput = [[AVCaptureDeviceInput alloc] initWithDevice:camera error:&error];
+	if (cameraInput == nil) {
+		NSLog(@"Error to create camera capture:%@",error);
+	}
+	
+	// Set the output
+	AVCaptureVideoDataOutput* videoOutput = [[AVCaptureVideoDataOutput alloc] init];
+	
+	// create a queue to run the capture on
+	dispatch_queue_t captureQueue=dispatch_queue_create("myQueue", NULL);
+	
+	// setup our delegate
+	[videoOutput setSampleBufferDelegate:self queue:captureQueue];
+
+	// configure the pixel format
+	videoOutput.videoSettings = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithUnsignedInt:kCVPixelFormatType_32BGRA], (id)kCVPixelBufferPixelFormatTypeKey,
+									 nil];
+
+	// and the size of the frames we want
+	//[session setSessionPreset:AVCaptureSessionPresetMedium];
+
+	// Add the input and output
+	[session addInput:cameraInput];
+	[session addOutput:videoOutput];
+	
+	// Start the session
+	[session startRunning];	
+
+
+
+
+
+
+   /* AVCaptureSession *session = [[AVCaptureSession alloc] init];
     
     CALayer *viewLayer = self.cameraFeed.layer;
     AVCaptureVideoPreviewLayer *captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
@@ -72,42 +141,24 @@
             }
         }
     }
-    /*
-     if (!frontCamera) {
-     NSError *error = nil;
-     AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:backCamera error:&error];
-     if (!input) {
-     NSLog(@"ERROR: trying to open front camera: %@", error);
-     }
-     [session addInput:input];
-     }
-     
-     if (frontCamera) {
-     NSError *error = nil;
-     AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:frontCamera error:&error];
-     if (!input) {
-     NSLog(@"ERROR: trying to open rear camera: %@", error);
-     }
-     [session addInput:input];
-     }
-     */
+
     
     NSError *error = nil;
     AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:backCamera error:&error];
 
     session.sessionPreset = AVCaptureSessionPresetMedium;
-    
 
-    
     [session addInput:input];
     
     
     
     ///Trying to create outputs
     ///According to Apple this is how we go about grabbing a still image.
+    */
     /*
      https://developer.apple.com/library/ios/documentation/AudioVideo/Conceptual/AVFoundationPG/AVFoundationPG.pdf
      */
+     /*
     AVCaptureVideoDataOutput  *output = [[AVCaptureVideoDataOutput alloc]  init];
     
     output.videoSettings = @{ (NSString *)kCVPixelBufferPixelFormatTypeKey : @(kCVPixelFormatType_32BGRA) };
@@ -132,30 +183,77 @@
     
 	[session startRunning];
     
-    
+    */
 }
 
 
+
+
+
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
+	
+    ////Again, from APPLE
+    if(processVideoFrame) {
+        /// They want you to implement this!
+        ////    https://developer.apple.com/library/ios/documentation/AudioVideo/Conceptual/AVFoundationPG/AVFoundationPG.pdf
+        ///
+        if(processVideoFrame){
+        NSLog(@"The process image system was called");
+         //  thumbNail = [self imageFromSampleBuffer:sampleBuffer];
+        
+    processVideoFrame =false;
+            
+            
+            
+            
+            CVImageBufferRef cvimgRef = CMSampleBufferGetImageBuffer(sampleBuffer);
+            // Lock the image buffer
+            CVPixelBufferLockBaseAddress(cvimgRef,0);
+            // access the data
+            int width=CVPixelBufferGetWidth(cvimgRef);
+            int height=CVPixelBufferGetHeight(cvimgRef);
+            // get the raw image bytes
+            uint8_t *buf=(uint8_t *) CVPixelBufferGetBaseAddress(cvimgRef);
+            size_t bprow=CVPixelBufferGetBytesPerRow(cvimgRef);
+            // turn it into something useful
+            thumbNail=createImage(buf, bprow, width, height);
+    
+            
+            [self performSelectorOnMainThread:@selector(updateThumbnail) withObject:nil waitUntilDone:NO];
+//        [self updateThumbnail];
+            
+        }
+    }
+	
+}
+
+
+
+//So the following func worked for a bit and then stopped. I don't know why. Typo?
+/*
 -(void)captureOutput:(AVCaptureOutput *)captureOutput didDropSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection{
    
-    NSLog(@"The capture output was called from the queue");
+
     
     ////Again, from APPLE
     if(processVideoFrame) {
-    /* They want you to implement this!
-     https://developer.apple.com/library/ios/documentation/AudioVideo/Conceptual/AVFoundationPG/AVFoundationPG.pdf
-     */
-        //processVideoFrame =false;
+    /// They want you to implement this!
+ ////    https://developer.apple.com/library/ios/documentation/AudioVideo/Conceptual/AVFoundationPG/AVFoundationPG.pdf
+     ///
+        processVideoFrame =false;
         NSLog(@"The process image system was called");
-    UIImage *image = [self imageFromSampleBuffer:sampleBuffer];
+ UIImage *image = [self imageFromSampleBuffer:sampleBuffer];
 
         //processVideoFrame =false;
-    [self subImage].image = image;
+   // [self subImage].image = image;
     }
 }
+*/
+
 
 - (IBAction)testTriggerButton:(id)sender {
-     processVideoFrame = true;
+    processVideoFrame = !processVideoFrame;
     NSLog(@"Button Press %x", processVideoFrame);
 }
 
@@ -165,9 +263,7 @@
     // This example assumes the sample buffer came from an AVCaptureOutput,
     //so its image buffer is known to be  a pixel buffer.
     NSLog(@"Image from sample buffer");
-    
-    
-    
+/*
     CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
     //Lock the base address of the pixel buffer.
     CVPixelBufferLockBaseAddress(imageBuffer,0);
@@ -192,9 +288,9 @@
             return nil;
         }
     }
-    
-    
-    // Get the base address of the pixel buffer.
+    */
+    /* *//*
+    Get the base address of the pixel buffer.
     void *baseAddress = CVPixelBufferGetBaseAddress(imageBuffer);
     // Get the data size for contiguous planes of the pixel buffer.
     size_t bufferSize = CVPixelBufferGetDataSize(imageBuffer);
@@ -214,9 +310,17 @@
     CGImageRelease(cgImage); 
     CVPixelBufferUnlockBaseAddress(imageBuffer, 0); 
     return image;
+    */
+    
 }
 
-
+-(void)updateThumbnail{
+    
+    NSLog(@"thumbNail is %f wide", thumbNail.size.width);
+    [self subImage].image = [self thumbNail];
+    //[[self subImage] setNeedsDisplay];
+//    subImage.image = [UIImage imageNamed:@"IsItGreen512.png"];
+}
 
 
 
